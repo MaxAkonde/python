01e19b4b5295904717a3fc0786036fc22f207ab7
MaxAkonde <devdevmaster@gmail.com>
2020-08-05 13:49:21 +0200

    07.all

---
+++ course_1_assignement_11/07_02.py
@@ -0,0 +1,12 @@
+wrds = ["end", 'work', "play", "start", "walk", "look", "open", "rain", "learn", "clean"]
+past_wrds = []
+
+for i in wrds:
+    i = i + "ed"
+    past_wrds.append(i)
+
+print(past_wrds)
+
+# For each string in wrds, add ‘ed’ 
+# to the end of the word (to make the word past tense). 
+# Save these past tense words to a list called past_wrds.
